e5325254afe0de08e58cb03816ae55e66f7fc2d6
Tsar Bomba Nick <njkuechel@protonmail.com>
2025-06-26 23:37:03 -0500

    Created SQLiteDB.get_query() and SQLiteDB.insert_query()

--- astrostash/astrostash.py
+++ astrostash/astrostash.py
@@ -1,6 +1,7 @@
 import pathlib as pl
 import os
 import sqlite3
+import pandas as pd
 import time
 from datetime import datetime
 import hashlib
@@ -47,6 +48,53 @@ class SQLiteDB:
         """
         with open("schema/base.sql", "r", encoding='utf-8') as schema:
             self.cursor.execute(schema.read())
+
+    def get_query(self, query_hash: str) -> pd.DataFrame:
+        """
+        Gets the query id (if it exists) based of the query parameters (hash)
+
+        Parameters:
+        query_hash: str, unique sha256 hash of the query
+
+        Returns:
+        pd.DataFrame, reference info for the query (if record exists)
+                      empty DataFrame if not queryied before
+        """
+        stashref = pd.read_sql("""SELECT * FROM queries
+                                  WHERE hash = :query_hash""",
+                               self.conn,
+                               params={"query_hash": query_hash})
+        return stashref
+
+    def insert_query(self, query_hash: str, refresh_rate: int) -> int:
+        """
+        Inserts info related to a query into the queries table
+
+        Parameters:
+        query: str, sha256 hash of the query parameters
+
+        refresh_rate: int, number of days since last query date to refresh
+                           database with fresh data
+
+        Returns:
+        int, id for the specific query
+        """
+        self.cursor.execute("""
+            INSERT INTO queries (
+                hash,
+                last_queried,
+                refresh_rate
+            )
+            VALUES (
+                :hash,
+                :last_queried,
+                :refresh_rate
+            );""", {"hash": query_hash,
+                    "last_queried": datetime.today().strftime('%Y-%m-%d'),
+                    "refresh_rate": refresh_rate}
+            )
+        self.conn.commit()
+        return self.cursor.lastrowid
 
     def close(self):
         """
--- astrostash/tests/test_astrostash_core.py
+++ astrostash/tests/test_astrostash_core.py
@@ -14,8 +14,18 @@ def test_sha256sum():
 
 def test_SQLiteDB():
     sql1 = astrostash.SQLiteDB()
-    sql1.close()
     assert pl.Path("astrostash.db").is_file() is True
+    # Test getting query that does not exist
+    qp = {"query": "PSR B0531+21",
+          "catalog": "nicermastr"}
+    qp_hash = astrostash.sha256sum(qp)
+    assert sql1.get_query(qp_hash).empty
+    # Test Insertion
+    id1 = sql1.insert_query(qp_hash, 14)
+    assert id1 == 1
+    # Test getting query that already exists
+    assert sql1.get_query(qp_hash).hash[0] == qp_hash
+    sql1.close()
     os.remove("astrostash.db")
     sql2 = astrostash.SQLiteDB(db_name="astrostash/tests/astrostash_test.db")
     sql2.close()
