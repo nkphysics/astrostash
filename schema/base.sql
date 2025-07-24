acc62c186b36c6c43d4e8a46db546c29d62cf14a
Tsar Bomba Nick <njkuechel@protonmail.com>
2025-07-23 19:01:31 -0500

    Added responses and query_response_pivot tables to base.sql

--- schema/base.sql
+++ schema/base.sql
@@ -1,8 +1,18 @@
 CREATE TABLE IF NOT EXISTS queries (
     id INTEGER PRIMARY KEY,
-    hash TEXT,
+    hash TEXT NOT NULL,
     last_queried DATE,
     refresh_rate INTEGER
-)
+);
 
+CREATE TABLE IF NOT EXISTS responses (
+    id INTEGER PRIMARY KEY,
+    hash TEXT NOT NULL
+);
 
+CREATE TABLE IF NOT EXISTS query_response_pivot (
+    queryid INTEGER,
+    responseid INTEGER,
+    FOREIGN KEY (queryid) REFERENCES queries(id),
+    FOREIGN KEY (responseid) REFERENCES responses(id)
+);
